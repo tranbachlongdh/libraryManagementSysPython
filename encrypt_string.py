938cc755f95eba1787b79f82a4fa293294b4eef8
tranbachlongdh <tranbachlongdh@gmail.com>
2019-07-17 00:30:59 +0700

    Update: Rename books.json to database.json remake encrypt_string module

--- encrypt_string.py
+++ encrypt_string.py
@@ -1,9 +1,10 @@
 import base64
 
 
+# Reverse string than encode by base64
 def encode_string(string):
-    return base64.b64encode(bytes(string, 'utf-8'))
+    return base64.b64encode(bytes(string[::-1], 'utf-8'))
 
 
 def decode_string(string):
-    return base64.b64decode(string).decode('utf-8')
+    return base64.b64decode(string).decode('utf-8')[::-1]
